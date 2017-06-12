ffc3cf49879b8ed312e4a9c5884d934e4dd6f793
Benoit Landry <ben.acc.landry@gmail.com>
2017-06-11 23:38:14 -0700

    full multiphase direct collocation

--- dircol/pusher_example.py
+++ dircol/pusher_example.py
@@ -12,13 +12,13 @@ interval_value = duration / (num_nodes - 1)
 
 # Symbolic equations of motion
 t = sym.symbols('t')
-x, y, theta, vpx, vpy, L1, L2 = sym.symbols('x, y, theta, vpx, vpy, L1, L2', cls=sym.Function)
+x, y, theta, vpx, vpy, L2, L3, L4, L5 = sym.symbols('x, y, theta, vpx, vpy, L2, L3, L4, L5', cls=sym.Function)
 
 state_symbols = (x(t), y(t), theta(t))
 specified_symbols = (vpx(t), vpy(t))
 
 def get_pushing_eom(rox1_0, roy1_0):
-    c = 1
+    c = 0.38259785823210635
     r1sq = rox1_0**2 + roy1_0**2
     rox1 = (sym.cos(theta(t))*rox1_0 - sym.sin(theta(t))*roy1_0)
     roy1 = (sym.sin(theta(t))*rox1_0 + sym.cos(theta(t))*roy1_0)
@@ -30,10 +30,12 @@ def get_pushing_eom(rox1_0, roy1_0):
                       theta(t).diff() - (rox1*vpy(t) - roy1*vpx(t))/(c*c + r1sq)])
     return eom1
 
-eom1 = get_pushing_eom(-0.5, 0.0)
 eom2 = get_pushing_eom(0.0, -0.5)
+eom3 = get_pushing_eom(0.5, 0.0)
+eom4 = get_pushing_eom(0.0, 0.5)
+eom5 = get_pushing_eom(-0.5, 0.0)
 
-eom = L1(t)*eom1 + L2(t)*eom2
+eom = L2(t)*eom2 + L3(t)*eom3 + L4(t)*eom4 + L5(t)*eom5
 
 def obj(free):
     """Minimize the sum of the squares of the control input."""
@@ -48,20 +50,34 @@ def obj_grad(free):
 # Specify the symbolic instance constraints, i.e. initial and end
 # conditions.
 instance_constraints = (x(0.0),
-                        x(duration) - 1.0,
+                        x(duration) - 3.0,
                         y(0.0),
                         y(duration) - 1.0,
                         theta(0.0),
                         theta(duration))
 
 bounds = {
-    vpx(t): (-10, 10),
-    vpy(t): (-10, 10),
+    vpx(t): (-1, 1),
+    vpy(t): (-1, 1),
+
+    ((vpx(t)*1.0 + vpy(t)*0.0)**2 - 0.030153689607045803*(vpx(t)**2 + vpy(t)**2)*1.0)*L2(t): (-float("inf"), 0),
+    (-vpx(t)*0.0 + vpy(t)*1.0)*L2(t): (0, float("inf")),
+
+    ((vpx(t)*0.0 + vpy(t)*1.0)**2 - 0.030153689607045803*(vpx(t)**2 + vpy(t)**2)*1.0)*L3(t): (-float("inf"), 0),
+    (-vpx(t)*1.0 + vpy(t)*0.0)*L3(t): (0, float("inf")),
+
+    ((vpx(t)*-1.0 + vpy(t)*0.0)**2 - 0.030153689607045803*(vpx(t)**2 + vpy(t)**2)*1.0)*L4(t): (-float("inf"), 0),
+    (-vpx(t)*0.0 + vpy(t)*-1.0)*L4(t): (0, float("inf")),
+
+    ((vpx(t)*0.0 + vpy(t)*-1.0)**2 - 0.030153689607045803*(vpx(t)**2 + vpy(t)**2)*1.0)*L5(t): (-float("inf"), 0),
+    (-vpx(t)*-1.0 + vpy(t)*0.0)*L5(t): (0, float("inf")),
 }
 
 known_trajectory_map = {
-    L1(t): np.concatenate([np.ones(num_nodes/2),np.zeros(num_nodes/2)]),
-    L2(t): np.concatenate([np.zeros(num_nodes/2),np.ones(num_nodes/2)])
+    L2(t): np.concatenate([np.ones(num_nodes/2),np.zeros(num_nodes/2)]),
+    L3(t): np.concatenate([np.zeros(num_nodes/2),np.zeros(num_nodes/2)]),
+    L4(t): np.concatenate([np.zeros(num_nodes/2),np.zeros(num_nodes/2)]),
+    L5(t): np.concatenate([np.zeros(num_nodes/2),np.ones(num_nodes/2)]),
 }
 
 # Create an optimization problem.
